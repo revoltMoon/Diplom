45034884ef1ecac3414a0eafea4ed28828e939a9
v.kupryakov <v.kupryakov@tinkoff.ru>
2020-05-12 19:15:24 +0300

    cleaning

--- Recognizer/ViewController.swift
+++ Recognizer/ViewController.swift
@@ -30,7 +30,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     func setupAVCapture() {
         var deviceInput: AVCaptureDeviceInput!
         
-        // Select a video device, make an input
         let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
         do {
             deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
@@ -40,9 +39,8 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         }
         
         session.beginConfiguration()
-        session.sessionPreset = .vga640x480 // Model image size is smaller.
+        session.sessionPreset = .vga640x480
         
-        // Add a video input
         guard session.canAddInput(deviceInput) else {
             print("Could not add video device input to the session")
             session.commitConfiguration()
@@ -51,7 +49,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         session.addInput(deviceInput)
         if session.canAddOutput(videoDataOutput) {
             session.addOutput(videoDataOutput)
-            // Add a video data output
             videoDataOutput.alwaysDiscardsLateVideoFrames = true
             videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
             videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
@@ -61,7 +58,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             return
         }
         let captureConnection = videoDataOutput.connection(with: .video)
-        // Always process the frames
         captureConnection?.isEnabled = true
         do {
             try  videoDevice!.lockForConfiguration()
@@ -84,7 +80,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         session.startRunning()
     }
     
-    // Clean up capture setup
     func teardownAVCapture() {
         previewLayer.removeFromSuperlayer()
         previewLayer = nil
@@ -95,7 +90,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // In the subclass
+        
     }
 }
 
